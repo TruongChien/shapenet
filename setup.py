4a23c33dbb0f8b85990f8c61e0a3b992f8686f0b
spyroot <spyroot@gmail.com>
2021-12-09 18:34:08 +0400

    fixing packing

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
+    version='1.0',
+    description='ShapeGen Graph Generator Toos',
+    author='Mustafa Bayramov',
+    author_email='spyroot@gmail.com',
     name='shapegnet',
     packages=find_packages(),
 )
